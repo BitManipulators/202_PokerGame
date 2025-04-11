b4459ba196e0358facb1369641a7366198ed0d7d
Brandon Jacklyn <brandonjacklyn@live.ca>
2025-04-11 14:28:44 -0700

    Set messagebox background color to green so text is visible

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    app.setStyleSheet("QMessageBox { background-color: #1a5c35; }");
     MainWindow window;
     window.show();
     return app.exec();
